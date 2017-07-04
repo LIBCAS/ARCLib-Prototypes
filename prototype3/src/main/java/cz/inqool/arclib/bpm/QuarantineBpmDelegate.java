b8a0f5d3bf3b010f21ee1d93b1ae8709a22e364d
Jan Tomášek <yan.tomasek@gmail.com>
2017-07-04 11:13:17 +0200

    spring tests, dependency injection, refactoring, camunda fix

--- prototype3/src/main/java/cz/inqool/arclib/bpm/QuarantineBpmDelegate.java
+++ prototype3/src/main/java/cz/inqool/arclib/bpm/QuarantineBpmDelegate.java
@@ -5,15 +5,18 @@ import cz.inqool.arclib.clamAV.ClamSIPAntivirusScanner;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
+@Component
 public class QuarantineBpmDelegate implements JavaDelegate {
 
-    protected SIPAntivirusScanner scanner;
+    private SIPAntivirusScanner scanner;
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
@@ -23,5 +26,10 @@ public class QuarantineBpmDelegate implements JavaDelegate {
         scanner.moveToQuarantine(infectedFiles.stream().map(
                 pathString -> Paths.get(pathString)
         ).collect(Collectors.toList()));
+    }
+
+    @Inject
+    public void setScanner(SIPAntivirusScanner scanner) {
+        this.scanner = scanner;
     }
 }
