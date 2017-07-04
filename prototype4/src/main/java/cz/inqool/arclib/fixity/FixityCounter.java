b80463b5c0ad0730b73d8574a150187ed956419c
Jan Tomášek <yan.tomasek@gmail.com>
2017-07-04 16:27:07 +0200

    javadoc

--- prototype4/src/main/java/cz/inqool/arclib/fixity/FixityCounter.java
+++ prototype4/src/main/java/cz/inqool/arclib/fixity/FixityCounter.java
@@ -9,8 +9,22 @@ import static cz.inqool.arclib.utils.Utils.notNull;
 
 public abstract class FixityCounter {
 
+    /**
+     * Abstract method to compute digest from a file. The type of digest depends on subclass implementation. Eg. MD5, CRC32, SHA-512 etc.
+     *
+     * @param pathToFile Path to file which digest has to be computed.
+     * @return byte array with computed digest
+     * @throws IOException
+     */
     public abstract byte[] computeDigest(Path pathToFile) throws IOException;
 
+    /**
+     * Computes digest for specified file and compares it with provided digest. The type of computed digest depends on subclass instance.
+     * @param pathToFile Path to file which digest has to be computed.
+     * @param expectedDigest Digest provided for comparison.
+     * @return true if digests matches, false otherwise
+     * @throws IOException
+     */
     public boolean verifyFixity(Path pathToFile, String expectedDigest) throws IOException {
         notNull(pathToFile, () -> {
             throw new IllegalArgumentException();
@@ -21,6 +35,13 @@ public abstract class FixityCounter {
         return expectedDigest.equals(bytesToHexString(computeDigest(pathToFile)));
     }
 
+    /**
+     * Computes digest for specified file and compares it with provided digest. The type of computed digest depends on subclass instance.
+     * @param pathToFile Path to file which digest has to be computed.
+     * @param expectedDigest Digest provided for comparison.
+     * @return true if digests matches, false otherwise
+     * @throws IOException
+     */
     public boolean verifyFixity(Path pathToFile, byte[] expectedDigest) throws IOException {
         notNull(pathToFile, () -> {
             throw new IllegalArgumentException();
