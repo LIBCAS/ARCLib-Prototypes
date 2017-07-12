e075e6410e7a9ba0cae9790649471962c3b4e536
Simon Hochla <simon.hochla@inqool.cz>
2017-07-12 14:17:03 +0200

    Comments added

--- arclib/prototype5/src/main/java/cz/inqool/arclib/mail/MailCenter.java
+++ arclib/prototype5/src/main/java/cz/inqool/arclib/mail/MailCenter.java
@@ -37,10 +37,24 @@ public class MailCenter {
 
     private String appName;
 
+    /**
+     * Send notification about the result of ingest.
+     * @param email email address of the recipient
+     * @param sipId id of the SIP package that has been processed
+     * @param result result message of the ingest
+     * @param created time when the notification was created
+     */
     public void sendIngestResultNotification(String email, String sipId, String result, Instant created) {
         sendNotificationInternal(email, sipId, result, created, "templates/ingestResultNotification.vm");
     }
 
+    /**
+     * Send notification on saving of an AIP package.
+     * @param email email address of the recipient
+     * @param sipId id of the SIP package that has been processed
+     * @param result result of the save operation
+     * @param created time when the notification was created
+     */
     public void sendAipSavedNotification(String email, String sipId, String result, Instant created) {
         sendNotificationInternal(email, sipId, result, created, "templates/aipSavedNotification.vm");
     }
