7ce2413b5cd3ac424dd8d97fd6b17bf166f5dd84
Jan Tomášek <yan.tomasek@gmail.com>
2017-07-12 11:38:37 +0200

    assertthrown, assertthat to all tests

--- prototype4/src/test/java/cz/inqool/arclib/FixityCounterTests.java
+++ prototype4/src/test/java/cz/inqool/arclib/FixityCounterTests.java
@@ -9,8 +9,10 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static helper.ThrowableAssertion.assertThrown;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+
 
 public class FixityCounterTests {
 
@@ -20,25 +22,27 @@ public class FixityCounterTests {
 
     @Before
     public void setUp() {
-        fixityCounter=new Md5FixityCounter();
+        fixityCounter = new Md5FixityCounter();
     }
 
     @Test
     public void testOK() throws IOException {
-        assertThat(fixityCounter.verifyFixity(PATH_TO_FILE,DIGEST),equalTo(true));}
-
-    @Test
-    public void testCorrupted() throws IOException{
-        String corruptedDigest = DIGEST.substring(1) + "1";
-        assertThat(fixityCounter.verifyFixity(PATH_TO_FILE,corruptedDigest),equalTo(false));}
-
-    @Test(expected= FileNotFoundException.class)
-    public void testNotFound() throws IOException {
-        fixityCounter.computeDigest(Paths.get("invalidpath"));
+        assertThat(fixityCounter.verifyFixity(PATH_TO_FILE, DIGEST), equalTo(true));
     }
 
-    @Test(expected= IllegalArgumentException.class)
+    @Test
+    public void testCorrupted() throws IOException {
+        String corruptedDigest = DIGEST.substring(1) + "1";
+        assertThat(fixityCounter.verifyFixity(PATH_TO_FILE, corruptedDigest), equalTo(false));
+    }
+
+    @Test
+    public void testNotFound() throws IOException {
+        assertThrown(() -> fixityCounter.computeDigest(Paths.get("invalidpath"))).isInstanceOf(FileNotFoundException.class);
+    }
+
+    @Test
     public void testNullPath() throws IOException {
-        fixityCounter.computeDigest(null);
+        assertThrown(() -> fixityCounter.computeDigest(null)).isInstanceOf(IllegalArgumentException.class);
     }
 }
